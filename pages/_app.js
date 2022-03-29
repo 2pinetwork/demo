14f0717de71ebce39574a87492a0944ba0e42b0b
Batudo <batudo@2pi.network>
2022-03-29 15:03:21 -0300

    React 18

--- pages/_app.js
+++ pages/_app.js
@@ -2,15 +2,11 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { ThemeProvider } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
-import Head from 'next/head'
 import { theme } from '@/theme'
 import { Provider } from '@/store'
 
 const App = ({ Component, pageProps }) => (
   <Provider>
-    <Head>
-      <meta name="viewport" content="width=device-width, initial-scale=1" />
-    </Head>
 
     <ThemeProvider theme={theme}>
       <CssBaseline />
--- pages/_document.js
+++ pages/_document.js
@@ -1,39 +1,34 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { Head, Html, Main, NextScript } from 'next/document'
 
-class CustomDocument extends Document {
-  static async getInitialProps(ctx) {
-    const initialProps = await Document.getInitialProps(ctx)
-    return { ...initialProps }
-  }
+const Document = () => {
+  return (
+    <Html>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-  render() {
-    return (
-      <Html>
-        <Head>
-          <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any" />
-          <link rel="icon" type="image/png" href="/favicon.png" sizes="64x64" />
-          <link rel="mask-icon" href="/favicon.svg" color="#000000" />
-          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta property="og:local" content="en_US" />
+        <meta property="og:title" content="2PI Demo" />
+        <meta property="og:description" content="2PI Demo" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://demo.2pi.network" />
+        <meta property="og:image" content="https://demo.2pi.network/images/2pi_512.png" />
 
-          <link rel="preconnect" href="https://fonts.googleapis.com" />
-          <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-          <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any" />
+        <link rel="icon" type="image/png" href="/favicon.png" sizes="64x64" />
+        <link rel="mask-icon" href="/favicon.svg" color="#000000" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
-          <meta property="og:local" content="en_US" />
-          <meta property="og:title" content="2PI Demo" />
-          <meta property="og:description" content="2PI Demo" />
-          <meta property="og:type" content="website" />
-          <meta property="og:url" content="https://demo.2pi.network" />
-          <meta property="og:image" content="https://demo.2pi.network/images/2pi_512.png" />
-        </Head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" />
+      </Head>
 
-        <body>
-          <Main />
-          <NextScript />
-        </body>
-      </Html>
-    )
-  }
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
 }
 
-export default CustomDocument
+export default Document
