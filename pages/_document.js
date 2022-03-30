438194317a1b91dfeb8979d282ec06e33154bd6d
Batudo <batudo@2pi.network>
2022-03-30 12:00:28 -0300

    Remove boilerplate from _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -1,32 +1,30 @@
 import { Head, Html, Main, NextScript } from 'next/document'
 
-const Document = () => {
-  return (
-    <Html>
-      <Head>
-        <meta property="og:local" content="en_US" />
-        <meta property="og:title" content="2PI Demo" />
-        <meta property="og:description" content="2PI Demo" />
-        <meta property="og:type" content="website" />
-        <meta property="og:url" content="https://demo.2pi.network" />
-        <meta property="og:image" content="https://demo.2pi.network/images/2pi_512.png" />
+const Document = () => (
+  <Html>
+    <Head>
+      <meta property="og:local" content="en_US" />
+      <meta property="og:title" content="2PI Demo" />
+      <meta property="og:description" content="2PI Demo" />
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://demo.2pi.network" />
+      <meta property="og:image" content="https://demo.2pi.network/images/2pi_512.png" />
 
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any" />
-        <link rel="icon" type="image/png" href="/favicon.png" sizes="64x64" />
-        <link rel="mask-icon" href="/favicon.svg" color="#000000" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+      <link rel="icon" type="image/svg+xml" href="/favicon.svg" sizes="any" />
+      <link rel="icon" type="image/png" href="/favicon.png" sizes="64x64" />
+      <link rel="mask-icon" href="/favicon.svg" color="#000000" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
 
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" />
-      </Head>
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,400;0,500;0,600;0,700;1,400&display=swap" />
+    </Head>
 
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
-}
+    <body>
+      <Main />
+      <NextScript />
+    </body>
+  </Html>
+)
 
 export default Document
